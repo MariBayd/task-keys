13fc0d406862fc5112130e1e96d094f922d50b68
MariBayd <masha.baydina@gmail.com>
2022-05-30 23:55:54 +0300

    Задание по спискам реакт

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,44 @@
+import { Interface } from 'readline';
 import { IItem } from './index';
+import { useState } from 'react';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    if (props.sorting === 'ASC') {
+        props.initialData.sort((a, b) => a.id - b.id);
+    } else props.initialData.sort((a, b) => b.id - a.id);
+
+    const [id, setID] = useState(0);
+    const [name, setName] = useState('');
+
+    return (
+        <ul>
+            {props.initialData.map((user) => {
+                if (user.id != id) {
+                    return (
+                        <li onClick={() => setID(user.id)} key={user.id}>
+                            {user.name}
+                        </li>
+                    );
+                } else {
+                    return (
+                        <input
+                            autoFocus={true}
+                            onChange={(e) => setName(e.currentTarget.value)}
+                            key={user.id}
+                            defaultValue={user.name}
+                            onKeyDown={(e) => {
+                                if (e.key == 'Escape') {
+                                    setID(0);
+                                }
+                                if (e.key == 'Enter') {
+                                    user.name = name;
+                                    setID(0);
+                                }
+                            }}
+                        />
+                    );
+                }
+            })}
+        </ul>
+    );
 }
